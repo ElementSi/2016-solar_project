6b1b2858bfa825cda12f0fbf3e16bf714b73209f
ElementSi <eg.zzz.747@gmail.com>
2022-04-05 17:19:35 +0300

    Fixed resolution

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += gravitational_constant * (body.m * obj.m / r**3) * abs(body.x-obj.x)
-        body.Fy += gravitational_constant * (body.m * obj.m / r**3) * abs(body.y-obj.y)
+        body.Fx += gravitational_constant * (body.m * obj.m / r**3) * (obj.x - body.x)
+        body.Fy += gravitational_constant * (body.m * obj.m / r**3) * (obj.y - body.y)
 
 
 
